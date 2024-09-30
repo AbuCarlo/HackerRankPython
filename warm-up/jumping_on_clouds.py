149db4972a0004f005f762d587ee3534b61e2730
Tony Nassar <anthonyabunassar@gmail.com>
2024-09-30 09:38:14 -0400

    Jumping on Clouds

---
+++ warm-up/jumping_on_clouds.py
@@ -0,0 +1,16 @@
+def jumpingOnClouds(c):
+    position = 0
+    jumps = 0
+    while position < len(c) - 1:
+        # The game is guaranteed to be winnable,
+        # so we can always jump from the second-to-
+        # last position to the very last. Otherwise,
+        # check if there's a forbidden cloud there;
+        # if not, go ahead and jump.
+        if position == len(c) -2 or c[position + 2]:
+            position += 1
+        else:
+            position += 2
+        jumps +=1
+    return jumps
+
