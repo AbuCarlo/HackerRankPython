4cbfca94eaa30b49939b45da0256f2edb33bd43b
Tony Nassar <anthonyabunassar@gmail.com>
2024-10-07 09:54:35 -0400

    Commented Luck Balance; renamed variables.

--- greedy/luck_balance.py
+++ greedy/luck_balance.py
@@ -1,6 +1,10 @@
+# https://www.hackerrank.com/challenges/luck-balance/problem
 
 sample_0 = [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]]
 
+# "m" contains memoized subproblems. 
+# "k" is, per HackerRank, "the number of important contests Lena can lose."
+# "c" is the remaining contests (i.e. some tail of the initial array).
 def recurse(m, k, c):
     if not c:
         return 0
@@ -13,10 +17,13 @@ def recurse(m, k, c):
         return result
     v, important = c[0]
     if not important:
+        # We can lose this contest with impunity.
         return v + recurse(m, k, c[1:])
-    x = v + recurse(m, k - 1, c[1:])
-    y = recurse(m, k, c[1:]) - v
-    result = max(x, y)
+    # "If Lena wins the contest, her luck balance will decrease by L[i];
+    # if she loses it, her luck balance will increase by L[i]."
+    if_lose = v + recurse(m, k - 1, c[1:])
+    if_win = recurse(m, k, c[1:]) - v
+    result = max(if_lose, if_win)
     m[key] = result
     return result
 
