f1f370b4e8ef954cbe2f95728153282d3205fe59
Tony Nassar <anthonyabunassar@gmail.com>
2024-10-16 12:58:59 -0400

    Passed Equal Stacks

--- problem-solving/data-structures/stacks/equal_stacks.py
+++ problem-solving/data-structures/stacks/equal_stacks.py
@@ -1,6 +1,9 @@
 '''
-
+https://www.hackerrank.com/challenges/equal-stacks/problem
 '''
+
+from collections import deque
+import heapq
 
 # Sample Test Case 0
 
@@ -11,13 +14,21 @@ sample = [
 def equalStacks(h1, h2, h3):
     '''
     '''
-    # pylint: disable=C0415
-    from collections import deque
-    stacks = [deque(h) for h in [h1, h2, h3]]
-    while not all(sum(s) == sum(stacks[0]) for s in stacks[1:]):
-        largest = max(stacks, key=sum)
-        largest.popleft()
+    # Python's heap is always a min-heap, so we have to invert the key.
+    # We only compute the sums once. Compute tuples: heapify() will
+    # use the first element of the tuple by default.
+    stacks = [(-sum(h), deque(h)) for h in [h1, h2, h3]]
+    heapq.heapify(stacks)
+    # Short-circuit check that the stacks are not the same size.
+    while not all(size == stacks[0][0] for size, _ in stacks[1:]):
+        key, stack = stacks[0]
+        # We don't have to recompute the sum; we already by
+        # how much we're reducing this stack. Remember that
+        # the keys are negative values.
+        key += stack[0]
+        stack.popleft()
+        heapq.heapreplace(stacks, (key, stack))
 
-    return sum(stacks[0])
-    
+    return -stacks[0][0]
+
 print(equalStacks(*sample))
