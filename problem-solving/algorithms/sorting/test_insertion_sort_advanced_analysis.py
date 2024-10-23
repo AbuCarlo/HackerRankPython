d56b0850313785d9d36f11e057e6a23d0a84d317
Tony Nassar <anthonyabunassar@gmail.com>
2024-10-22 23:13:22 -0400

    Added new stub implementation.

---
+++ problem-solving/algorithms/sorting/test_insertion_sort_advanced_analysis.py
@@ -0,0 +1,14 @@
+'''
+https://www.hackerrank.com/challenges/insertion-sort/problem
+'''
+
+# pylint: disable=C0103
+def insertionSort(arr):
+    '''
+    Count the number of swaps that an insertion sort on this 
+    array would require.
+    
+    :param a: an unsorted array to be sorted
+    :returns: the number of swaps
+    '''
+    return 0
