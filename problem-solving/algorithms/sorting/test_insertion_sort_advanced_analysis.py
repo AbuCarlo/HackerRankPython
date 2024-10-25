153d755ce82bcb6661b1a5d3921567f54a40c943
Tony Nassar <anthonyabunassar@gmail.com>
2024-10-25 19:06:23 -0400

    Dead with BST?

--- problem-solving/algorithms/sorting/test_insertion_sort_advanced_analysis.py
+++ problem-solving/algorithms/sorting/test_insertion_sort_advanced_analysis.py
@@ -2,85 +2,12 @@
 https://www.hackerrank.com/challenges/insertion-sort/problem
 '''
 
+import collections
+from collections.abc import Iterable
+
 import hypothesis
 import hypothesis.strategies
 import pytest
-
-# Let's invent a data structure: (n, count, total_count)
-# total_count will be the sum of the total_count fields
-# of the two children + the sum of each child's count.
-
-def check_heap_invariant(h: list, i: int):
-    left, right = 2 * i + 1, 2 * i + 2
-    actual = 0
-    if left < len(h) and h[left] is not None:
-        actual += h[left][1] + h[left][2]
-    if right < len(h) and h[right] is not None:
-        actual += h[right][1] + h[left][2]
-    assert actual == h[i][2]
-
-def heap_find(h: list, n: int):
-    result = 0
-    i = 0
-    odd = (n % 2) == 1
-    while True:
-        # The value is not in the heap yet.
-        if i >= len(h) or h[i] is None or n > h[i][0]:
-            return None
-        check_heap_invariant(h, i)
-        if h[i][0] == n:
-            # The value is in the heap
-            _, previous_count, previous_total_count = h[i]
-            h[i] = (n, previous_count + 1, previous_total_count)
-            return result
-        else:
-            result += h[i][2]
-            sub_x, sub_y = 2 * i + 1, 2 * i + 2
-            result += h[i][1]
-            if not odd:
-                sub_x, sub_y = sub_y, sub_x
-            i = sub_x
-            if sub_y < len(h) and h[sub_y] is not None:
-                result -= h[sub_y][2]
-
-
-def heap_insert(h: list, n: int):
-    '''
-    Count the number of elements > n, leftward of n:
-    this is the number of swaps before n reaches its
-    correct position on this iteration.
-    '''
-    found = heap_find(h, n)
-    if found is not None:
-        return found
-
-    result = 0
-    i = 0
-    while True:
-        if i >= len(h):
-            # 0, 1, 3, 7, 15, ...
-            h.extend([None] * (len(h) + 1))
-        # We've never encountered n.
-        if h[i] is None:
-            # Implement the swap algorithm.
-            h[i] = (n, 1, 0)
-            return result
-        elif h[i][1] == n:
-            _, previous_count, previous_total_count = h[i]
-            h[i] = (n, previous_count + 1, previous_total_count)
-            return result
-        else:
-            # Add the current element as 1, plus
-            # the sum of the sizes of its two subtrees.
-            result += h[i][2] + 1
-            # The two subtrees of h[i]
-            subtree, other = 2 * i + 1, 2 * i + 2
-            assert h[i][2] == h[subtree][2] + h[other][2]
-            if n % 2 == 0:
-                subtree, other = other, subtree
-            # Subtract the size of the subtree we're *not* following.
-            result -= h[other][2]
-            i = subtree
 
 # pylint: disable=C0103
 def insertionSort(a):
@@ -91,11 +18,83 @@ def insertionSort(a):
     :param a: an unsorted array to be sorted
     :returns: the number of swaps
     '''
-    h = []
+    counts = collections.defaultdict(int)
     swaps = 0
-    for i in range(1, len(a)):
-        swaps += heap_insert(h, a[i])
+    bst = make_bst(sorted(set(a)))
+
+    for n in a:
+        i = 0
+        while bst[i] != n:
+            # Does n have to swap with a larger value?
+            if n < bst[i]:
+                swaps += counts[bst[i]]
+            i = 2 * i + 1 if n < bst[i] else 2 * i + 2
+        counts[n] += 1
     return swaps
+
+def calculate_tree_size(n: int) -> int:
+    '''
+    A perfectly balanced tree will have a
+    size 2^n - 1 for some n. We want to round
+    len() up to a power of 2, unless it's
+    already a power of 2, in which case
+    we need to round it up one more time.
+    '''
+    assert n > 0
+    # 1 represents an edge case: it's 1 less than
+    # a power of two, and it's also a power of two.
+    # But we know that we can represent a single
+    # node as an array of length 1.
+    if n == 1:
+        return 1
+    bits = n.bit_length()
+    if n == 1 << bits:
+        bits += 1
+    result = (1 << bits) - 1
+    return result
+
+def make_bst(l: Iterable[int]):
+    '''
+    Turn a sorted array of unique values into a binary search tree
+    to allow counting instances of n in a in log(len(a)) time.
+    '''
+    if not l:
+        return []
+    size = calculate_tree_size(len(l))
+    root = size // 2
+    width = root + 1
+    bst = [None] * size
+    bst[0] = l[root]
+    i = 0
+    while width > 1:
+        for j in range((width // 2) - 1, size, width):
+            # This happens on the first iteration.
+            if j == root:
+                continue
+            bst[i + 1] = l[j]
+            i += 1
+        width //= 2
+    return bst
+
+def traverse_bst(bst: list[int]):
+    '''
+    Return a new list representing the depth-first
+    traversal of the BST.
+    '''
+    if not bst:
+        return []
+    l = []
+
+    def traverse_bst_internal(i: int):
+        if i >= len(bst):
+            return
+        traverse_bst_internal(2 * i + 1)
+        l.append(bst[i])
+        traverse_bst_internal(2 * i + 2)
+
+    traverse_bst_internal(0)
+
+    return l
 
 samples = [
     ([1, 1, 1, 2, 2], 0),
@@ -136,52 +135,24 @@ def test_unique(a):
     assert True
 
 
-def make_bst(l: list[int]):
-    if not l:
-        return []
-    # A perfectly balanced tree will have a
-    # size 2^n - 1 for some n. We want to round
-    # len() up to a power of 2, unless it's
-    # already a power of 2, in which case
-    # we need to round it up one more time.
-    size = (1 << (len(l) + 1).bit_length() - 1) - 1
-    root = size // 2
-    width = (size + 1) // 2
-    bst = [0] * len(l)
-    bst[0] = l[root]
-    i = 0
-    while width > 1:
-        for j in range((width // 2) - 1, len(l), width):
-            # This happens on the first iteration.
-            if j == root:
-                continue
-            bst[i + 1] = l[j]
-            i += 1
-        width //= 2
-    return bst
-
-
-def traverse_bst_internal(bst: list[int], i: int, l: list[int]):
-    if i >= len(bst):
-        return
-    traverse_bst_internal(bst, 2 * i + 1, l)
-    l.append(bst[i])
-    traverse_bst_internal(bst, 2 * i + 2, l)
-
-
-def traverse_bst(bst: list[int]):
-    if not bst:
-        return []
-    l = []
-    traverse_bst_internal(bst, 0, l)
-    return l
-
 @hypothesis.given(hypothesis.strategies.lists(hypothesis.strategies.integers(min_value=1, max_value=10000000), min_size=1, max_size=100000))
-def test_bst_round_trip(l: list[int]):
+def test_sorted_input(l: list[int]):
+    '''
+    Any already-sorted list should require 0 swaps.
+    '''
+    l.sort()
+    actual = insertionSort(l)
+    assert actual == 0
+
+
+@hypothesis.given(l=unique_integers())
+def test_bst_round_trip(l):
     '''
     Verify that an BST represents the original array by 
     depth-first traversal. The limits above are from the 
-    original problem.
+    original problem. We've implemented the BST to *count*
+    duplicates, so we have to start from an array of 
+    unique values.
     '''
     l.sort()
     bst = make_bst(l)
@@ -191,14 +162,48 @@ def test_bst_round_trip(l: list[int]):
 SAMPLES = [
     ([], []),
     ([1], [1]),
+    ([1,2], [2, 1]),
     ([1, 2, 3], [2, 1, 3]),
     (list(range(1, 8)), [4, 2, 6, 1, 3, 5, 7]),
     (list(range(1, 16)), [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15])
 ]
 
 @pytest.mark.parametrize("l,bst", SAMPLES)
-
 def test_bst_simple(l: list[int], bst: list[int]):
-    # Edge cases, and simple versions that I worked out with paper and pencil.
+    '''
+    Edge cases, and simple versions that I worked out with paper and pencil.
+    '''
     assert traverse_bst(bst) == l
     assert make_bst(l) == bst
+
+
+HACKER_RANK_SAMPLES = [
+    # samples
+    ([1, 1, 1, 2, 2], 0),
+    ([2, 1, 3, 1, 2], 4)
+    # test cases
+]
+
+@pytest.mark.parametrize("l,expected", HACKER_RANK_SAMPLES)
+def test_test_cases(l, expected):
+    '''
+    Samples and test cases from HackerRank
+    '''
+    actual = insertionSort(l)
+    assert actual == expected
+    
+TREE_SIZE_SAMPLES = [
+    (1, 1),
+    (2, 3),
+    (3, 3),
+    (4, 7),
+    (7, 7)
+]
+
+@pytest.mark.parametrize("n, expected", TREE_SIZE_SAMPLES)
+def test_tree_size(n, expected):
+    '''
+    Verify calculate_tree_size()
+    '''
+    actual = calculate_tree_size(n)
+    assert actual == expected
