ef1452cc22c91416798eab69b3b628771c4422cd
Tony Nassar <anthonyabunassar@gmail.com>
2024-10-28 20:29:50 -0400

    Solved!

--- interview-preparation/search/test_connected_cell_in_grid.py
+++ interview-preparation/search/test_connected_cell_in_grid.py
@@ -6,49 +6,67 @@ import collections
 
 import pytest
 
+# pylint: disable=C0103
 def connectedCell(matrix):
-    neighbors = {}
+    '''
+    Solution implemented as directed graph, from which disjoint sets
+    were created. The size of the largest disjoint set is the answer.
+    '''
+    adjacency = collections.defaultdict(set)
     for column in range(len(matrix[0])):
         if matrix[0][column] == 0:
             continue
         key = (0, column)
+        adjacency[key].add(key)
         if column < len(matrix[0]) - 1 and matrix[0][column + 1] == 1:
             # Point this cell at the following one.
-            neighbors[key] = (0, column + 1)
-        else:
-            neighbors[key] = key
-    for column in range(len(matrix[0]) - 2, -1, -1):
-        if matrix[0][column] == 0:
-            continue
-        key = (0, column)
-        if (0, column + 1) in neighbors:
-            neighbors[key] = neighbors[(0, column + 1)]
+            adjacency[key].add((0, column + 1))
     for row in range(1, len(matrix)):
         for column in range(0, len(matrix[row])):
             if matrix[row][column] == 0:
                 continue
             key = (row, column)
+            adjacency[key].add(key)
             if column > 0 and matrix[row - 1][column - 1] == 1:
-                neighbors[key] = neighbors[(row - 1, column - 1)]
-            elif matrix[row - 1][column] == 1:
-                neighbors[key] = neighbors[(row - 1, column)]
-            elif column < len(matrix[row]) - 1 and matrix[row - 1][column + 1] == 1:
-                neighbors[key] = neighbors[(row - 1, column + 1)]
-            elif column < len(matrix[row]) - 1 and matrix[row][column + 1] == 1:
-                neighbors[key] = (row, column + 1)
-            else:
-                neighbors[key] = key
-        for column in range(len(matrix[0]) - 2, -1, -1):
-            if matrix[row][column] == 0:
-                continue
-            if (row, column + 1) in neighbors:
-                neighbors[(row, column)] = neighbors[(row, column + 1)]
+                adjacency[key].add((row - 1, column - 1))
+            if matrix[row - 1][column] == 1:
+                adjacency[key].add((row - 1, column))
+            if column < len(matrix[row]) - 1 and matrix[row - 1][column + 1] == 1:
+                adjacency[key].add((row - 1, column + 1))
+            if column < len(matrix[row]) - 1 and matrix[row][column + 1] == 1:
+                adjacency[key].add((row, column + 1))
 
+    # Conveniently, we can count the 1s this way.
+    adjacency_size = sum([sum(row) for row in matrix])
+    assert len(adjacency) == adjacency_size
+
+    parents = {}
     disjoint_sets = collections.defaultdict(set)
-    for cell, parent in neighbors.items():
-        disjoint_sets[parent].add(cell)
+
+    for cell in adjacency:
+        parents[cell] = cell
+        disjoint_sets[cell] = set([cell])
+
+    def _find_root(cell):
+        parent = parents[cell]
+        while parent != parents[parent]:
+            parent, parents[parent] = parents[parent], parents[parents[parent]]
+        return parent
+
+    for cell, neighbors in adjacency.items():
+        for neighbor in neighbors:
+            x = _find_root(cell)
+            y = _find_root(neighbor)
+            if x == y:
+                continue
+
+            parents[y] = x
+            disjoint_sets[x] |= disjoint_sets[y]
+            disjoint_sets.pop(y)
+
     sizes = [len(children) for children in disjoint_sets.values()]
     return max(sizes)
+
 
 _HACKER_RANK_SAMPLES = [
     # This is the example from the text. Their answer is wrong.
@@ -56,7 +74,18 @@ _HACKER_RANK_SAMPLES = [
     # Sample Test Case 2
     ([[1, 1, 0, 0, 0], [0, 1, 1, 0, 0], [0, 0, 1, 0, 1], [1, 0, 0, 0, 1], [0, 1, 0, 1, 1]], 5),
     # Test Case 1
-    ([[0, 0, 1, 1], [0, 0, 1, 0], [0, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]], 8)
+    ([[0, 0, 1, 1], [0, 0, 1, 0], [0, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]], 8),
+    # Test Case 6
+    ([
+        [0, 1, 0, 0, 0, 0, 1, 1, 0],
+        [1, 1, 0, 0, 1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0, 1, 0, 1, 1],
+        [0, 1, 1, 1, 0, 0, 1, 1, 0],
+        [0, 1, 0, 1, 1, 0, 1, 1, 0],
+        [0, 1, 0, 0, 1, 1, 0, 1, 1],
+        [1, 0, 1, 1, 1, 1, 0, 0, 0]
+    ], 29)
 ]
 
 @pytest.mark.parametrize("grid,expected", _HACKER_RANK_SAMPLES)
