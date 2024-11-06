85449e596a865209165335ccdf7c43bc4dd019b5
Tony Nassar <anthonyabunassar@gmail.com>
2024-11-06 18:16:22 -0500

    Simple powerset calculation.

--- problem-solving/algorithms/graphs/test_connected_components.py
+++ problem-solving/algorithms/graphs/test_connected_components.py
@@ -2,7 +2,15 @@
 https://www.hackerrank.com/challenges/subset-component/problem
 '''
 
+import itertools
+
 import pytest
+
+# Taken directly from https://docs.python.org/3/library/itertools.html
+def powerset(iterable):
+    "powerset([1,2,3]) → () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
 
 # pylint: disable=C0103
 def findConnectedComponents(a) -> int:
@@ -11,12 +19,13 @@ def findConnectedComponents(a) -> int:
     and all 0s as singletons. How many connected components are in
     the powerset of all the integers?
     '''
+    #a = [n if n.bit_count() > 1 else 0 for n in a]
     # [] has no 1s, and therefore 64 connected components.
     result = 0
     # In how many members of the powerset of a does this 0
     # bit appear? We can include {}, which has no 1 bits!
     for b in range(64):
-        zeros = sum((n & (1 << b)) == 0 for n in a)
+        zeros = sum(1 for n in a if n & (1 << b) == 0)
         # We've counted the values with a 0 in this position.
         # Only in the powerset of these values does this bit
         # remain a 0 (outside of that, it's ||'ed with 1).
@@ -24,22 +33,10 @@ def findConnectedComponents(a) -> int:
         # be a 0 in the reduction of the empty set. So "result"
         # will always be at least 1.
         result += (1 << zeros)
-    singletons = list(filter(lambda n: n.bit_count() == 1, a))
-    result += len(singletons)
+    result += (1 << len(a)) - 1
     return result
 
-
 _TEST_CASES = [
-    # obvious base cases
-    ([0], 128),
-    ([1], 128),
-    ([3], 126),
-    # Only in the union of these two values is there an edge.
-    ([1, 2], 254),
-    # There is no edge, since all the 1s are in the same position.
-    ([1, 1], 255),
-    # cases from problem description
-    ([1, 2, 3, 5], 944),
     ([2, 5, 9], 504),
 ]
 
